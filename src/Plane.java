cbb24c486a305ef359dc1a37ed77bb029261a2a9
Savera <Level 2>
2016-01-07 18:32:20 -0800

    ArrayList
    ArrayList

--- src/Plane.java
+++ src/Plane.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Plane {
-
+	public int numberOfPassengers;
+	Random r = new Random(); 
+public static ArrayList <SnakesOnAPlane> Snakes = new ArrayList <SnakesOnAPlane>();
+	public static void main(String[] args) {
+		
+	}
+	Plane() {
+		for (int r = 0; i > 100; i++) {
+			new SnakesOnAPlane(r, r);
+		}
+	}
 }
--- src/SnakesOnAPlane.java
+++ src/SnakesOnAPlane.java
@@ -1,7 +1,31 @@
 import java.util.Random;
 
 public class SnakesOnAPlane {
+	int viciousness;
+	boolean venomous;
 	public static void main(String[] args) {
 		
 	}
+
+	SnakesOnAPlane(int viciousness, boolean venomous) {
+		this.viciousness = viciousness;
+		this.venomous = venomous;
+	}
+
+	public int getViciousness() {
+		return viciousness;
+	}
+
+	public void setViciousness(int viciousness) {
+		this.viciousness = viciousness;
+	}
+
+	public boolean isVenomous() {
+		return venomous;
+	}
+
+	public void setVenomous(boolean venomous) {
+		this.venomous = venomous;
+	}
+
 }
