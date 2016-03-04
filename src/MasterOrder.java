f1c0f3e122841e236671cc253db710e616000a9d
league <league@macbook-pro.attlocal.net>
2016-03-03 18:28:58 -0800

    ArrayList Practice

--- src/MasterOrder.java
+++ src/MasterOrder.java
@@ -1,24 +1,52 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MasterOrder {
-ArrayList<CookieOrder> Orders = new ArrayList<CookieOrder>();
-public ArrayList<CookieOrder> getOrders() {
-	return Orders;
+	ArrayList<CookieOrder> Orders = new ArrayList<CookieOrder>();
+
+	public ArrayList<CookieOrder> getOrders() {
+		return Orders;
+	}
+
+	public void setOrders(ArrayList<CookieOrder> orders) {
+		Orders = orders;
+	}
+
+	MasterOrder() {
+		Orders = new ArrayList();
+
+	}
+
+	public void addOrder(CookieOrder cookieOrder) {
+		Orders.add(cookieOrder);
+
+	}
+
+	public int getTotalBoxes() {
+		int numOrder = 0;
+		for (CookieOrder cookieOrder : Orders) {
+			numOrder += cookieOrder.getNumBoxes();
+		}
+		return numOrder;
+	}
+
+public int removeVariety(String string) {
+	int RemovedBoxes = 0; 
+		Orders.iterator();
+		Iterator<CookieOrder> bob = Orders.iterator();
+		while (bob.hasNext()) {
+			CookieOrder test = bob.next();
+			if (test.getVariety().equals(string)) {
+				RemovedBoxes += test.numBoxes;
+				bob.remove();
+			}
+			
+		}
+		return RemovedBoxes; 
+	}
+
+
+
 }
 
-public void setOrders(ArrayList<CookieOrder> orders) {
-	Orders = orders;
-}
 
-MasterOrder() {
-Orders = new ArrayList(); 
-
-}
-
-public void addOrder(CookieOrder cookieOrder) {
-Orders.add(cookieOrder);	
-
-	
-}
-
-}
