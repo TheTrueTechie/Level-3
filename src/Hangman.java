d1e4a26ce4a8c674d262f004b7e3f871e31ac7b0
League Mac <league@WTS15.attlocal.net>
2016-04-07 18:31:43 -0700

    Hangman
    Fixed

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,6 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -7,22 +8,35 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Hangman implements KeyListener {
-	Stack<String> hangmanStack = new Stack<String>();
-
+	 Stack<String> hangmanStack = new Stack<String>();
+	 ArrayList<JLabel> labels = new ArrayList<JLabel>();
+	 
 	public JFrame mainFrame = new JFrame();
 	public JPanel mainPanel = new JPanel();
-	public JLabel Label1 = new JLabel();
+	//public JLabel Label1 = new JLabel();
 
 	private String TITLE = "Hangman";
 	private String currentPuzzle;
-	private int HEIGHT = 400;
-	private int WIDTH = 200;
+	private int HEIGHT = 800;
+	private int WIDTH = 1000;
 
 	public static void main(String[] args) {
 		Hangman a = new Hangman();
 		a.addPuzzles();
 		a.createUI();
+		a.currentPuzzle = a.hangmanStack.pop();
+		a.playPuzzle();
+	}
 
+	private void playPuzzle() {
+	
+		for (char s : currentPuzzle.toCharArray()) {
+			JLabel label2 = new JLabel();
+			label2.setText("_");
+			labels.add(label2);
+			mainPanel.add(label2);
+		}
+		
 	}
 
 	public void createUI() {
@@ -32,29 +46,29 @@ public class Hangman implements KeyListener {
 		mainFrame.setTitle(TITLE);
 		mainFrame.setSize(WIDTH, HEIGHT);
 
-		mainPanel.add(Label1);
+	//	mainPanel.add(Label1);
 
 		mainFrame.addKeyListener(this);
 		mainFrame.setVisible(true);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		this.keyTyped(null);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		String a = ""; 
-		currentPuzzle = hangmanStack.pop();
-		for (int i = 0; i < currentPuzzle.length(); i++) {
-			a += "-";
-			System.out.println(i);
-			
-		}
-		Label1.setText(a);
+		
+for (int i = 0; i < currentPuzzle.length(); i++) {
+	
+}
+	if (a==e.getKeyChar()) {
+		
+	}
+	
+	
+}
 
 		
 
-	}
+	
 
 	@Override
 	public void keyPressed(KeyEvent e) {
